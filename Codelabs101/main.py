902057831408cf14925865d5073cc055ee4020f4
Nyakundi <henrymoire@gmail.com>
2023-09-28 19:20:12 +0300

    Ordered files
    Moved the codelabs101 into it's own folder. Split the codelabslol.py into main.py and functions.py

---
+++ Codelabs101/main.py
@@ -0,0 +1,36 @@
+import openpyxl
+import re
+from functions import *
+
+path = r'D:\Bachelor of Science in Informatics and Computer Science\Year 3\Semester 2\Computer Graphics\Code\Repository\ComputerGraphics\Codelabs101\Test Files.xlsx'
+
+# Define variable to load the dataframe
+dataframe = openpyxl.load_workbook(path)
+ 
+# Define variable to read sheet
+dataframe1 = dataframe.active
+
+emails = create_emails(dataframe1)
+no_repeat_emails = unique_email(emails)
+if (no_repeat_emails):
+    print("True")
+else:
+    print("False")
+# print("The email addresses are:")
+# for x in emails:
+#     print(x)
+
+# print("")
+
+# male_students = list_male(dataframe1)
+# print("The male students are:")
+# for x in male_students:
+#     print(x)
+
+# print("")
+
+# female_students = list_female(dataframe1)
+# print("The female students are:")
+# for x in female_students:
+#     print(x)
+
